b7a60fd217ae915ee55dde530609e14973a15f42
nikoz84 <nikoz.1984@gmail.com>
2019-09-17 18:03:23 -0300

    adicionar package.json

--- includes/configs.php
+++ includes/configs.php
@@ -2,13 +2,36 @@
 
 return [
     'servicos' => [
-        ['id' => 1, 'title' => 'CONSTRUÇÃO', 'options' => []],
-        ['id' => 2, 'title' => 'REFORMA', 'options' => []],
-        ['id' => 3, 'title' => 'PROJETOS', 'options' => []],
-        ['id' => 4, 'title' => 'PAINEL SOLAR', 'options' => []],
-        ['id' => 5, 'title' => 'AUTOMAÇÃO RESIDENCIAL', 'options' => []],
-        ['id' => 6, 'title' => 'INSPEÇÃO PREDIAL', 'options' => []],
-        ['id' => 7, 'title' => 'ORÇAMENTO E PLANEJAMENTO', 'options' => []]
+        ['id' => 1, 'title' => 'CONSTRUÇÃO', 'options' => [
+            'question' => 'Possui Projeto?',
+            'tipo_emprendimentos' => [
+                ['name' => 'Residencial', 'types' => ['Casa', 'Edificios', 'Quitinetes']],
+                ['name' => 'Comercial', 'types' => ['Empresarial', 'Ponto Comercial - Logas', 'Mall']],
+                ['name' => 'Outros', 'types' => []]
+            ]
+        ]],
+        ['id' => 2, 'title' => 'REFORMA', 'options' => [
+            'question' => 'Possui Projeto?'
+        ]],
+        ['id' => 3, 'title' => 'PROJETOS', 'options' => [
+            'question' => 'Possui Projeto?'
+        ]],
+        ['id' => 4, 'title' => 'PAINEL SOLAR', 'options' => [
+            'question' => 'Você está com sua conta de luz?'
+        ]],
+        ['id' => 5, 'title' => 'AUTOMAÇÃO RESIDENCIAL', 'options' => [
+            'question' => 'Possui Projeto?'
+        ]],
+        ['id' => 6, 'title' => 'INSPEÇÃO PREDIAL', 'options' => [
+            'tipo_emprendimentos' => [
+                ['name' => 'Residencial', 'types' => ['Casa', 'Edificios', 'Quitinetes']],
+                ['name' => 'Comercial', 'types' => ['Empresarial', 'Ponto Comercial - Logas', 'Mall']],
+                ['name' => 'Outros', 'types' => []]
+            ]
+        ]],
+        ['id' => 7, 'title' => 'ORÇAMENTO E PLANEJAMENTO', 'options' => [
+            'question' => 'Possui Projeto?'
+        ]]
     ],
     'tipo_projeto' => [
         ['name' => 'Arquitetura', 'types' => []],
